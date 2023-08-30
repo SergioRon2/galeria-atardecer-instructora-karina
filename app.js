ce7da57381aaa2d51019c6245eb138fb8dfc9e0d
SergioRon2 <rondonjr2005@gmail.com>
2023-08-30 10:50:04 -0500

    30/08/2023

---
+++ app.js
@@ -0,0 +1,9 @@
+function informacionImagen(){
+    let webgrafia = 'Esta imagen salio de la pagina pexels.'
+    document.getElementById('img-function').innerHTML = webgrafia;
+}
+function borrarInfo(){
+    let borrar = ' '
+    document.getElementById('img-function').innerHTML = borrar;
+}
+
